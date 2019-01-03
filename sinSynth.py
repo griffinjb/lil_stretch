19d90e933a61875ec4cbf343d2d3796ada6b7638
griffinjb <griffinj@oregonstate.edu>
2019-01-03 13:53:33 -0800

    imports and stuff

--- sinSynth.py
+++ sinSynth.py
@@ -1,2 +1,19 @@
 # sin bank synthesizer
 
+from impIMG import *
+
+
+
+
+class parameters:
+	windowSize 		= 100   # Samples
+	freqInterval 	= 1 	# Hz
+
+
+def main():
+	print('yo!')
+
+
+if __name__ == "__main__":
+	parm = parameters()
+	main()
